903c10011ffcadb7e20ccebe17f73200a3e6b81f
Marcelo Bezerra <71769279+MarceloBBatista@users.noreply.github.com>
2021-09-22 14:20:24 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace MEDIA_ALUNOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.btnmedia = new System.Windows.Forms.Button();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.lblmedia = new System.Windows.Forms.Label();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(54, 105);
+            this.txt1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(148, 26);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(54, 160);
+            this.txt2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(148, 26);
+            this.txt2.TabIndex = 1;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(54, 212);
+            this.txt3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(148, 26);
+            this.txt3.TabIndex = 2;
+            // 
+            // btnmedia
+            // 
+            this.btnmedia.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnmedia.Location = new System.Drawing.Point(53, 320);
+            this.btnmedia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnmedia.Name = "btnmedia";
+            this.btnmedia.Size = new System.Drawing.Size(112, 32);
+            this.btnmedia.TabIndex = 3;
+            this.btnmedia.Text = "Média";
+            this.btnmedia.UseVisualStyleBackColor = true;
+            this.btnmedia.Click += new System.EventHandler(this.btnmedia_Click);
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(54, 44);
+            this.txtnome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(361, 26);
+            this.txtnome.TabIndex = 4;
+            // 
+            // lblmedia
+            // 
+            this.lblmedia.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblmedia.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblmedia.Location = new System.Drawing.Point(51, 273);
+            this.lblmedia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblmedia.Name = "lblmedia";
+            this.lblmedia.Size = new System.Drawing.Size(364, 23);
+            this.lblmedia.TabIndex = 5;
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnlimpar.Location = new System.Drawing.Point(186, 320);
+            this.btnlimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(112, 32);
+            this.btnlimpar.TabIndex = 6;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnsair.Location = new System.Drawing.Point(316, 320);
+            this.btnsair.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(112, 32);
+            this.btnsair.TabIndex = 7;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(54, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(57, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nota 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(54, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nota 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(57, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nota 3:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::MEDIA_ALUNOS.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(461, 410);
+            this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.lblmedia);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.btnmedia);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Média Dos Alunos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btnmedia;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.Label lblmedia;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MEDIA_ALUNOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void btnmedia_Click(object sender, EventArgs e)
+        {
+            double N1=0, N2=0, N3=0, Media=0;
+            N1 = double.Parse(txt1.Text);
+            N2 = double.Parse(txt2.Text);
+            N3 = double.Parse(txt3.Text);
+            Media = Math.Round((N1 + N2 + N3) / 3,2);
+            lblmedia.Text = txtnome.Text + " sua média é: " + Convert.ToString(Media);
+
+            if (Media >=6)
+            {
+                lblmedia.ForeColor = Color.Green;
+                lblmedia.Text = lblmedia.Text + " Você foi APROVADO";
+            }
+            else
+            {
+                lblmedia.ForeColor = Color.Red;
+                lblmedia.Text = lblmedia.Text + " Você foi REPROVADO";
+            }
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtnome.Clear();
+            txt1.Clear();
+            txt2.Clear();
+            txt3.Clear();
+            lblmedia.Text = "";
+            txtnome.Focus();
+        }
+    }
+}
